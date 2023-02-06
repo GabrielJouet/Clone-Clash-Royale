409ab06465ca5512c626e7ddc097ce62ab9af871
Gabriel Jouet <gabriel.jouet@viacesi.fr>
2023-02-06 14:33:34 +0100

    Added the first unit

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -8,7 +8,7 @@ public class Unit : MonoBehaviour
     /// <summary>
     /// Health of this unit, set to 0 it will die.
     /// </summary>
-    [SerializeField]
+    [SerializeField, Range(2, 50)]
     private int _health;
 
     /// <summary>
@@ -20,36 +20,49 @@ public class Unit : MonoBehaviour
     /// <summary>
     /// Speed of this unit.
     /// </summary>
-    [SerializeField]
+    [SerializeField, Range(0.5f, 2)]
     private float _speed;
 
     /// <summary>
     /// Attack speed of this unit.
     /// </summary>
-    [SerializeField]
+    [SerializeField, Range(0.25f, 1)]
     private float _attackSpeed;
 
     /// <summary>
     /// Attack damage of this unit.
     /// </summary>
-    [SerializeField]
+    [SerializeField, Range(1, 10)]
     private int _attackDamage;
 
     /// <summary>
     /// Minimum range before an unit saw another unit and try to kill it.
     /// </summary>
-    [SerializeField]
+    [SerializeField, Range(0.5f, 2)]
     private float _seeRange;
 
     /// <summary>
     /// Minimum range before an unit can attack another.
     /// </summary>
-    [SerializeField]
+    [SerializeField, Range(0.1f, 1)]
     private float _attackRange;
 
     /// <summary>
     /// Mana cost of this unit.
     /// </summary>
-    [SerializeField]
+    [SerializeField, Range(1, 10)]
     private int _manaCost;
+
+
+    /// <summary>
+    /// Does this unit is flying?
+    /// </summary>
+    [SerializeField]
+    private bool _flying;
+
+    /// <summary>
+    /// Does this unit ignore other units?
+    /// </summary>
+    [SerializeField]
+    private bool _ignoreOponents;
 }
